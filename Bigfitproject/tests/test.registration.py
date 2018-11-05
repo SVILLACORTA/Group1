bb4f4a9779c9befd3dbaec6d59975bc62eba1560
alhiadaria6122 <43358132+alhiadaria6122@users.noreply.github.com>
2018-11-04 19:47:31 -0800

    Create test.registration.py

---
+++ Bigfitproject/tests/test.registration.py
@@ -0,0 +1,14 @@
+from django.contrib.import User
+from django.contrib.sites. import Site
+from django.core import mail
+from django.test import TestCase
+from registration.models import RegistrationProfile
+class TestRegistration(TestCase):
+    def test_activation_email(self):
+        site = Site.objects.get()
+        user = User(email='test@example.com')
+        profile = RegistrationProfile(user=user, activation_key='activation-key')
+        profile.send_activation_email(site)
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox.pop()
+        self.assertEqual(message.subject, 'Activate your Bigfitproject.com account')
