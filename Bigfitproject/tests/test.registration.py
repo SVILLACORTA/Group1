9f3d5ff9c79e340f1be85ada1b513267439f5dd4
alhiadaria6122 <43358132+alhiadaria6122@users.noreply.github.com>
2018-11-06 17:41:39 -0800

    Alaa

--- Bigfitproject/tests/test.registration.py
+++ Bigfitproject/tests/test.registration.py
@@ -1,14 +1,12 @@
-from django.contrib.import User
-from django.contrib.sites. import Site
-from django.core import mail
 from django.test import TestCase
-from registration.models import RegistrationProfile
-class TestRegistration(TestCase):
-    def test_activation_email(self):
-        site = Site.objects.get()
-        user = User(email='test@example.com')
-        profile = RegistrationProfile(user=user, activation_key='activation-key')
-        profile.send_activation_email(site)
-        self.assertEqual(len(mail.outbox), 1)
-        message = mail.outbox.pop()
-        self.assertEqual(message.subject, 'Activate your Bigfitproject.com account')
+
+from ..forms import SignUpForm
+
+
+class SignUpFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2', ]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
+        # this will test the form of the signup when our client try to signup for the account.
