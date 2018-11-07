0ab77d935064a0ccab8bef501c89595b3f3c5979
PunkShadow925 <punkshadow925@gmail.com>
2018-11-07 09:27:23 -0800

    GUI pages by Seher-Uploaded by Yi in 11/07

--- Bigfitproject/tests/test.registration.py
+++ Bigfitproject/tests/test.registration.py
@@ -1,18 +1,3 @@
-from django.test import TestCase
-<<<<<<< HEAD
-from registration.models import RegistrationProfile
-
-class TestRegistration(TestCase):
-    def test_activation_email(self):
-        site = Site.objects.get()
-        user = User(email='test@example.com')
-        profile = RegistrationProfile(user=user, activation_key='activation-key')
-        profile.send_activation_email(site)
-        self.assertEqual(len(mail.outbox), 1)
-        message = mail.outbox.pop()
-        self.assertEqual(message.subject, 'Activate your Bigfitproject.com account')
-=======
-
 from ..forms import SignUpForm
 
 
@@ -23,4 +8,3 @@ class SignUpFormTest(TestCase):
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
         # this will test the form of the signup when our client try to signup for the account .
->>>>>>> 1e04d072b0e49b7d117fb9913cf761ea40655a04
